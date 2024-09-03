65c025de1d4e3993bda3755967d1754caa5ca6bd
nope <nope@gisadan.dev>
2024-09-02 21:56:40 -0400

    add search bar

--- web/src/lib/components/dictionary-page/dictionary-page.svelte
+++ web/src/lib/components/dictionary-page/dictionary-page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+    import { goto } from '$app/navigation'
     import type {
         DefinitionDto,
         ExampleDto,
         PartialDefinition
     } from '$lib/api/dtos'
+    import ArrowRight from '$lib/icons/arrow-right.svelte'
+    import { draw } from 'radash'
     import AppHeader from '../app-header/app-header.svelte'
+    import Button from '../ui/button/button.svelte'
+    import Input from '../ui/input/input.svelte'
     import Definitions from './definitions-preview.svelte'
     import ExamplesPreview from './examples-preview.svelte'
     import PartialDefinitionsPreview from './partial-definitions-preview.svelte'
@@ -15,9 +20,54 @@
     export let partialDefinitions: Record<string, PartialDefinition>
     export let examples: ExampleDto[]
     export let exampleCount: Promise<number>
+
+    const placeholders = [
+        '안녕하세요',
+        '15세 이상 감상을 권장합니다',
+        '방과 후 전쟁활동',
+        '모기전쟁',
+        '잔불의 기사',
+        '네가 있는 마을',
+        '거꾸로 핀 꽃'
+    ]
+
+    let formEl: HTMLFormElement
+
+    function onSubmit() {
+        const formData = new FormData(formEl)
+
+        let newQuery = formData.get('search') as string | null
+        newQuery = (newQuery ?? '').trim()
+        if (newQuery) {
+            goto(`/dictionary?query=${newQuery}`)
+        }
+    }
 </script>
 
 <AppHeader />
+
+<form
+    on:submit|preventDefault={onSubmit}
+    bind:this={formEl}
+    class="flex items-center px-6 pt-8 pb-4"
+>
+    <!-- <Label for="search" class="text-lg font-bold">Search</Label> -->
+    <Input
+        name="search"
+        class="h-12 text-xl border-primary bg-muted rounded-r-none rounded-l-xl"
+        placeholder={query || draw(placeholders)}
+        value={query}
+    />
+
+    <Button
+        type="submit"
+        class="h-12 w-16 p-0 rounded-l-none rounded-r-xl"
+    >
+        <ArrowRight
+            class="p-3.5 stroke-[2px] stroke-primary-foreground"
+        />
+    </Button>
+</form>
 
 <div class="flex flex-col gap-8 p-4 pb-16">
     <Definitions {query} {definitions} count={definitionCount} />
