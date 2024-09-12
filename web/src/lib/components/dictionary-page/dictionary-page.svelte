c14a2f87e7b257e4bef3344eb064b4076c281fa0
nope <nope@gisadan.dev>
2024-09-11 20:39:11 -0400

    link to real dictionaries

--- web/src/lib/components/dictionary-page/dictionary-page.svelte
+++ web/src/lib/components/dictionary-page/dictionary-page.svelte
@@ -6,11 +6,13 @@
         PartialDefinition
     } from '$lib/api/dtos'
     import ArrowRight from '$lib/icons/arrow-right.svelte'
+    import { euc } from '$lib/miscUtils'
     import { draw } from 'radash'
     import Button from '../ui/button/button.svelte'
     import Input from '../ui/input/input.svelte'
     import Definitions from './definitions-preview.svelte'
     import ExamplesPreview from './examples-preview.svelte'
+    import ExternalSearchButton from './external-search-button.svelte'
     import PartialDefinitionsPreview from './partial-definitions-preview.svelte'
 
     export let query: string
@@ -49,32 +51,61 @@
     }
 </script>
 
-<form
-    on:submit|preventDefault={onSubmit}
-    bind:this={formEl}
-    class="flex items-center px-6 pt-8 pb-4"
->
-    <Input
-        name="search"
-        class="h-12 text-xl border-primary bg-muted rounded-r-none rounded-l-xl"
-        placeholder={query || draw(placeholders)}
-        value={query}
-    />
-
-    <Button
-        type="submit"
-        class="h-12 w-16 p-0 rounded-l-none rounded-r-xl"
+<div class="flex flex-col">
+    <form
+        on:submit|preventDefault={onSubmit}
+        bind:this={formEl}
+        class="flex items-center px-6 pt-8"
     >
-        <ArrowRight
-            class="p-3.5 stroke-[2px] stroke-primary-foreground"
+        <Input
+            name="search"
+            class="h-12 text-xl border-primary bg-muted rounded-r-none rounded-l-xl"
+            placeholder={query || draw(placeholders)}
+            value={query}
         />
-    </Button>
-</form>
 
-<div class="flex flex-col gap-8 p-4 pb-16">
-    <Definitions {query} {definitions} count={definitionCount} />
+        <Button
+            type="submit"
+            class="h-12 w-16 p-0 rounded-l-none rounded-r-xl"
+        >
+            <ArrowRight
+                class="p-3.5 stroke-[2px] stroke-primary-foreground"
+            />
+        </Button>
+    </form>
 
-    <ExamplesPreview {query} {examples} count={exampleCount} />
+    {#if query.trim().length}
+        <div class="pt-3 pb-4 px-6 flex gap-2 flex-wrap">
+            <ExternalSearchButton
+                label="Naver"
+                href="https://en.dict.naver.com/#/search?query={euc(
+                    query
+                )}"
+            />
 
-    <PartialDefinitionsPreview {query} {partialDefinitions} />
+            <ExternalSearchButton
+                label="Wiktionary"
+                href="https://en.wiktionary.org/w/index.php?search={euc(
+                    query
+                )}"
+            />
+
+            <ExternalSearchButton
+                label="krdict"
+                href="https://krdict.korean.go.kr/eng/dicMarinerSearch/search?nation=eng&nationCode=6&ParaWordNo=&mainSearchWord={euc(
+                    query
+                )}"
+            />
+        </div>
+    {:else}
+        <div class="pt-4"></div>
+    {/if}
+
+    <div class="flex flex-col gap-8 px-4 pt-8 pb-16">
+        <Definitions {query} {definitions} count={definitionCount} />
+
+        <ExamplesPreview {query} {examples} count={exampleCount} />
+
+        <PartialDefinitionsPreview {query} {partialDefinitions} />
+    </div>
 </div>
